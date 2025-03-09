3acc471c9b8e04106ed927eda6fce6cb8cba0e64
RexTsou <rex89x@gmail.com>
2025-03-09 13:01:00 +0800

    update cloudSQL service for show table data

--- CloudSQL/cloudrun-sql-app/main.py
+++ CloudSQL/cloudrun-sql-app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from sqlalchemy import create_engine, text
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 # 連線 Cloud SQL
 DB_USER = "DB_USER_NAME"
@@ -14,7 +16,8 @@ db_url = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}
 engine = create_engine(db_url)
 
 @app.get("/")
-def read_root():
+def read_root(request: Request):
     with engine.connect() as conn:
-        result = conn.execute(text("SELECT 'Hello from Cloud SQL!'")).fetchone()
-        return {"message": result[0]}
+        result = conn.execute(text("SELECT id, name, email FROM user_database.users")).fetchall()
+    
+    return templates.TemplateResponse("index.html", {"request": request, "users": result})
