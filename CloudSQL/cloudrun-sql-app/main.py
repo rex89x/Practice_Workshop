5ee24abab423b7951e2106b0ab1ffefdccc7b6b5
RexTsou <rex89x@gmail.com>
2025-03-09 16:47:54 +0800

    commit dockerfile, requirements, update main.py

--- CloudSQL/cloudrun-sql-app/main.py
+++ CloudSQL/cloudrun-sql-app/main.py
@@ -1,6 +1,9 @@
+import os
 from fastapi import FastAPI, Request
 from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError
 from fastapi.templating import Jinja2Templates
+import pymysql
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -13,11 +16,21 @@ DB_HOST = "DB_HOST"
 DB_PORT = "3306" # My SQL Port
 
 db_url = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(db_url)
+engine = create_engine(db_url, pool_pre_ping=True)
 
 @app.get("/")
 def read_root(request: Request):
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT id, name, email FROM user_database.users")).fetchall()
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT id, name, email FROM user_database.users")).fetchall()
+    except OperationalError as e:
+        return {"error": "Database connection failed", "details": str(e)}
     
     return templates.TemplateResponse("index.html", {"request": request, "users": result})
+
+
+# 讓 Cloud Run 使用 PORT 8080
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
